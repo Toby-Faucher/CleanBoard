99759f7ea89ef1953c1c8b9380b52e2a0aaa5c4e
Toby Faucher <toby@tfaucher.com>
2025-08-27 12:58:37 -0600

    feat: implemented the config.py to work with the main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .config import settings
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
